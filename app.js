80e976b5ca359aa4c5c6b6ca13284f3a44fd2223
jazzman1825 <valerfaust@gmail.com>
2021-05-01 16:17:36 +0300

    word-wrap implemented

--- app.js
+++ app.js
@@ -1,65 +1,64 @@
-
- const productList = [
-{
-    model: 'Diamond mattress',
-    image: '1.jpg',
-    price:'30',
-    Id: 0,
-    description:'<p>Less than a year old mattress</p><p> In good condition, intact and clean</p><p>Great quality, it was just too thick for us.</p> <p>Length 200 cm</p><p>Width 80 cm</p><p>Height 18 cm</p>',
-    date: '20/03/2020',
-    contact: 'Linda Henkinen',
-    place: 'Tuira, Oulu',
+const productList = [
+    {
+        model: 'Diamond mattress',
+        image: '1.jpg',
+        price:'30',
+        Id: 0,
+        description:'Less than a year old mattress In good condition, intact and clean Great quality, it was just too thick for us. Length 200 cm Width 80 cm Height 18 cm',
+        date: '20/03/2020',
+        contact: 'Linda Henkinen',
+        place: 'Tuira, Oulu',
+        },
+    {
+        model: 'Mirror',
+        price: '100',
+        image: '2.jpg',
+        Id: 1,
+        description:'Nice and big mirror in the wooden frame Length 100 cmHeight 180 cm', 
+        date: '20/03/2020',
+        contact: 'Lily Pad',
+        place: 'Linnanmaa, Oulu',
+        },
+    {    
+        model: 'Diamond mattress',
+        image: '1.jpg',
+        price:'30',
+        Id: 2,
+        description:'Less than a year old mattress In good condition, intact and cleanGreat quality, it was just too thick for us. Length 200 cmWidth 80 cmHeight 18 cm',
+        date: '20/03/2020',
+        contact: 'Linda Henkinen',
+        place: 'Tuira, Oulu',
     },
-{
-    model: 'Mirror',
-    price: '100',
-    image: '2.jpg',
-    Id: 1,
-    description:'<p>Nice and big mirror in the wooden frame</p><p> Length 100 cm</p><p>Height 180 cm</p>', 
-    date: '20/03/2020',
-    contact: 'Lily Pad',
-    place: 'Linnanmaa, Oulu',
-    },
-{    
-    model: 'Diamond mattress',
-    image: '1.jpg',
-    price:'30',
-    Id: 2,
-    description:'<p>Less than a year old mattress</p><p> In good condition, intact and clean</p><p>Great quality, it was just too thick for us.</p> <p>Length 200 cm</p><p>Width 80 cm</p><p>Height 18 cm</p>',
-    date: '20/03/2020',
-    contact: 'Linda Henkinen',
-    place: 'Tuira, Oulu',
-},
-{
-    model: 'Mirror',
-    image: '2.jpg',
-    price: '100',
-    Id: 3,
-    description:'<p>Nice and big mirror in the wooden frame</p><p> Length 100 cm</p><p>Height 180 cm</p>', 
-    date: '20/03/2020',
-    contact: 'Lily Pad',
-    place: 'Linnanmaa, Oulu',
-    },
-{
-    model: 'Diamond mattress',
-    image: '1.jpg',
-    price:'30',
-    Id: 4,
-    description:'<p>Less than a year old mattress</p><p> In good condition, intact and clean</p><p>Great quality, it was just too thick for us.</p> <p>Length 200 cm</p><p>Width 80 cm</p><p>Height 18 cm</p>',
-    date: '20/03/2020',
-    contact: 'Linda Henkinen',
-    place: 'Tuira, Oulu',
-    },
-{
-    model: 'Mirror',
-    image: '2.jpg',
-    price: '100',
-    Id: 5,
-    description:'<p>Nice and big mirror in the wooden frame</p><p> Length 100 cm</p><p>Height 180 cm</p>', 
-    date: '20/03/2020',
-    contact: 'Lily Pad',
-    place: 'Linnanmaa, Oulu',
-    },
+    {
+        model: 'Mirror',
+        image: '2.jpg',
+        price: '100',
+        Id: 3,
+        description:'Nice and big mirror in the wooden frame Length 100 cmHeight 180 cm', 
+        date: '20/03/2020',
+        contact: 'Lily Pad',
+        place: 'Linnanmaa, Oulu',
+        },
+    {
+        model: 'Diamond mattress',
+        image: '1.jpg',
+        price:'30',
+        Id: 4,
+        description:'Less than a year old mattress In good condition, intact and cleanGreat quality, it was just too thick for us. Length 200 cmWidth 80 cmHeight 18 cm',
+        date: '20/03/2020',
+        contact: 'Linda Henkinen',
+        place: 'Tuira, Oulu',
+        },
+    {
+        model: 'Mirror',
+        image: '2.jpg',
+        price: '100',
+        Id: 5,
+        description:'Nice and big mirror in the wooden frame Length 100 cmHeight 180 cm', 
+        date: '20/03/2020',
+        contact: 'Lily Pad',
+        place: 'Linnanmaa, Oulu',
+        },
 ];
 
 function createHomePage() {
@@ -77,11 +76,11 @@ for (x = 0; x < productList.length; x++) {
                     
     <div><img class="product-photo" src="Images/${productList[x].image}"/></div>
                    
-        <div class="produt-text">
+        <div>
                         
-        <p class="product-name">${productList[x].model}</p>
+        <p class="product-name">${productList[x].model}
 
-        <p>${productList[x].description}</p>
+        <p class ="product-text">${productList[x].description}
 
         </div>
 <div>
@@ -94,9 +93,9 @@ for (x = 0; x < productList.length; x++) {
                             
 <span class="product-published"> ${productList[x].date}</span>
     
-<p class="product-published"> ${productList[x].place}</p> </p>
+<p class="product-published"> ${productList[x].place} 
 
-<p class="contact-info"></i>${productList[x].contact}</p>
+<p class="contact-info"></i>${productList[x].contact}
 
 <button class="buy-btn" onclick="passKey(${x});"><a href="product_page.html"> Buy</a></button>
     
@@ -118,7 +117,7 @@ function createProductPage(id) {
 
     <div class="product-description">
 
-       <p class="product-name-description"></p>
+       <p class="product-name-description">
 
            <img class="product-photo-description" src="Images/${productList[id].image}"/>
 
@@ -129,7 +128,7 @@ function createProductPage(id) {
               ${productList[id].description} 
 
    
-                   <p class="contact-name-description">Contact: ${productList[id].contact}</p>
+                   <p class="contact-name-description">Contact: ${productList[id].contact}
    
                    <button class="contact-btn" onclick="showContacts('8 (981) 106-20-83');">Show contact</button>
    
@@ -137,11 +136,11 @@ function createProductPage(id) {
    
                <div>
    
-                    <p class="product-price-description">  ${productList[id].price}  &#8364;</p>
+                    <p class="product-price-description">  ${productList[id].price}  &#8364;
                        
-                    <p class="product-published-description">  ${productList[id].date}</p>
+                    <p class="product-published-description">  ${productList[id].date}
    
-                    <p class="product-published-description"> ${productList[id].place} </p>
+                    <p class="product-published-description"> ${productList[id].place} 
                
                </div>
    
